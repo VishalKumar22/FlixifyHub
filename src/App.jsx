d9910ef2e43f07ef66b9d8ff11d84ce043745e79
VishalKumar22 <vishalr2227@gmail.com>
2024-02-01 03:52:41 +0530

    Add Netflix Files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () =>{
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/signup' element={<Signup />}></Route>
+      <Route path='/profile' element={<Profile />}></Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
